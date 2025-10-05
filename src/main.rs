22805b156b451dc75af7c0b7bc610dea4bf2ac25
NotGhoull <58859813+NotGhoull@users.noreply.github.com>
2025-10-05 20:42:30 +0100

    feat: Add a simple window

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod core;
 mod binary;
 
 use async_trait::async_trait;
+use iced::{executor, widget::{button, column, text, Column}, Application};
 use log::info;
 use tokio::sync::watch;
 use traits::ModProvider;
@@ -12,8 +13,6 @@ use std::{sync::Arc, time::SystemTime};
 use crate::{core::{DefaultDownloadService, DownloadService, ProviderApi}, providers::ModWorkShopProvider};
 
 fn setup_logger() -> Result<(), fern::InitError> {
-
-
     fern::Dispatch::new()
         .format(|out, message, record| {
             out.finish(format_args!(
@@ -31,7 +30,7 @@ fn setup_logger() -> Result<(), fern::InitError> {
     Ok(())
 }
 #[tokio::main]
-async fn main() {
+async fn main() -> iced::Result {
     setup_logger().unwrap();
 
     info!("Welcome back to development hell");
@@ -40,10 +39,34 @@ async fn main() {
     let api = PApi::new(shared_download_service).into_arc();
 
     let loaded_provider = ModWorkShopProvider::new(Arc::clone(&api));
+
+    iced::application("Test title", update, view)
+        .theme(|_| iced::Theme::Dark)
+        .centered()
+        .run()
+
     // dbg!(loadedProvider.configure());
-    loaded_provider.download_mod("mod_id".to_string()).await;
+    // loaded_provider.download_mod("mod_id".to_string()).await;
+
 
 }
+
+#[derive(Debug, Clone)]
+enum Message {
+    Increment
+}
+fn update(value: &mut u64, message: Message) {
+    match message {
+        Message::Increment => *value += 1,
+    }
+}
+
+fn view(value: &u64) -> Column<Message> {
+    column![
+        text("Hello, Void world!").size(32),
+    ]
+}
+
 
 pub struct PApi {
     download_service: Arc<dyn DownloadService>
